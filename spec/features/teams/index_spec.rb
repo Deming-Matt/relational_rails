b4872a388ba6778144f17138525005e835b19fa6
Matt Deming <vballmatt44@gmail.com>
2022-05-12 12:05:09 -0600

    Create test, route, and view for user story 1

--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -7,9 +7,10 @@ RSpec.describe 'team index page', type: :feature do
   #When I visit '/parents'
   #Then I see the name of each parent record in the system
   it "can see the name of each team" do
+    team = Team.create!(name: "Colorado Avalanche", roster_spots: 23, full_roster: true)
 
-    visit '/team'
-require "pry"; binding.pry
+    visit '/teams'
+# require "pry"; binding.pry
     expect(page).to have_content("Club: #{team.name}")
   end
 end

f95da1c9018ad2a4a3393bb140ab40ba6f4191fe
Matt Deming <vballmatt44@gmail.com>
2022-05-15 20:23:23 -0600

    Add tests and functionality for user story 7

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,8 @@
 class TeamsController < ApplicationController
   def index
-    @team = Team.all
+    @team = Team.all.order(created_at: :desc)
   end
-
+  
   def show
     @team = Team.find(params[:id])
   end
--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -23,4 +23,23 @@ RSpec.describe 'team index page', type: :feature do
     expect(page).to have_content("Roster Spots Filled: #{coav.roster_spots}")
     expect(page).to have_content("Is their roster full? #{coav.full_roster}")
   end
+
+  #User story 6
+  it 'can show the records in the teams database are ordered by most recently created first' do
+    derw = Team.create!(name: "Detriot Red Wings", roster_spots: 22, full_roster: false, created_at: 5.second.ago)
+    coav = Team.create!(name: "Colorado Avalanche", roster_spots: 23, full_roster: true, created_at: 1.second.ago)
+    miwi = Team.create!(name: "Minnesota Wild", roster_spots: 23, full_roster: true, created_at: 10.second.ago)
+    stlb = Team.create!(name: "St. Louis Blues", roster_spots: 21, full_roster: false, created_at: 4.second.ago)
+    visit '/teams'
+    # require "pry"; binding.pry
+    expect(page.text.index("#{coav.name}")).to be < page.text.index("#{stlb.name}")
+    expect(page.text.index("#{stlb.name}")).to be < page.text.index("#{derw.name}")
+    expect(page.text.index("#{derw.name}")).to be < page.text.index("#{miwi.name}")
+  end
+
+  #User story 7
+  it 'can show the count of the number of players associated with that team' do
+
+    
+  end
 end

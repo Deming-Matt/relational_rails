01f806237309546b6297eb338895a0a3c5683fbb
Matt Deming <vballmatt44@gmail.com>
2022-05-17 22:24:01 -0600

    Add model test and method is_healthy

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,9 @@ class Player < ApplicationRecord
   validates_presence_of :lname
   validates_presence_of :jersey
   validates_inclusion_of :healthy, :in => [true, false]
+
+  def self.is_healthy
+      Player.where(healthy: true)
+  end
+  
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -13,4 +13,16 @@ describe Player, type: :model do
     it { should allow_value(false).for(:healthy)}
   end
 
+  describe "healthy" do
+    it "will show only players that are healthy" do
+      coav = Team.create!(name: "Colorado Avalanche", roster_spots: 23, full_roster: true)
+      nathan = coav.players.create!(fname: "Nathan", lname: "MacKinnon", jersey: 29, healthy: true)
+      gabriel = coav.players.create!(fname: "Gabriel", lname: "Landeskog", jersey: 92, healthy: true)
+      darcy = coav.players.create!(fname: "Darcy", lname: "Kuemper", jersey: 35, healthy: false)
+      jack = coav.players.create!(fname: "Jack", lname: "Johnson", jersey: 3, healthy: true)
+
+      expect(Player.is_healthy).to eq([nathan, gabriel, jack])
+    end
+  end
+
 end
